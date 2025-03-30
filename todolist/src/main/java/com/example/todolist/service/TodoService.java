b8366b2d40333059c17d3443e5aa94f683f9261d
André Ribeiro <andreor@gmail.com>
2025-03-30 17:19:59 -0300

    Initial commit

---
+++ todolist/src/main/java/com/example/todolist/service/TodoService.java
@@ -0,0 +1,48 @@
+package com.example.todolist.service;
+
+
+import com.example.todolist.entity.Todo;
+import com.example.todolist.repository.TodoRepository;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class TodoService {
+
+    private final TodoRepository todoRepository;
+
+    public TodoService(TodoRepository todoRepository) {
+        this.todoRepository = todoRepository;
+    }
+
+
+    public List<Todo> create(Todo todo) {
+        todoRepository.save(todo);
+        return list();
+    }
+
+
+    public List<Todo> list() {
+        Sort sortBy = Sort.by(Sort.Order.asc("priority"));
+        return todoRepository.findAll(sortBy);
+    }
+
+
+
+    public List<Todo> delete(Long id)  {
+       todoRepository.deleteById(id);
+       return list();
+    }
+
+    public List<Todo> update(Todo newTodo)  {
+        todoRepository.save(newTodo);
+        return list();
+    }
+
+
+
+}
